6c5a00960db9d412e4b5c0561f4b21d4a8071107
Cecilia Accettura <131543388+ChechitaAccettura@users.noreply.github.com>
2024-05-29 11:03:25 -0300

    JSON en firebase y JS externo

--- JSMuySimple.js
+++ JSMuySimple.js
@@ -1,24 +1,24 @@
-// Hacer una solicitud para obtener los datos de la API
-function obtenerCursos() { //obtener los datos de la base en realtime
-    fetch('https://my-first-project-con-potrero-default-rtdb.firebaseio.com/cursos')
-      .then(response => response.json())
-      .then(data => {
-        // Actualizar la lista de cursos en la página
-        const cursosLista = document.getElementById('cursos-lista'); //limpia el elemento para que siempre comience vacío
-        cursosLista.innerHTML = '';
+ // Hacer una solicitud para obtener los datos de la API
+ function obtenerCursos() { //obtener los datos de la base en realtime
+  fetch('https://my-first-project-con-potrero-default-rtdb.firebaseio.com/cursos.json')
+    .then(response => response.json())
+    .then(data => {
+      // Actualizar la lista de cursos en la página
+      const cursosLista = document.getElementById('cursos-lista'); //limpia el elemento para que siempre comience vacío
+      cursosLista.innerHTML = '';
 
-        for (const curso in data) {
-          const li = document.createElement('li');
-          li.textContent = data[curso].nombre + ' - ' + '$' + data[curso].valor;
+      for (const curso in data) {
+        const li = document.createElement('li');
+        li.textContent = data[curso].nombre + ' - ' + '$' + data[curso].valor;
 
-          //crea los elementos HTML
-          cursosLista.appendChild(li);
-        }
-      });
-  }
+        //crea los elementos HTML
+        cursosLista.appendChild(li);
+      }
+    });
+}
 
-  // ejecuta la función para obtener los datos por primera vez
-  obtenerCursos();
+// ejecuta la función para obtener los datos por primera vez
+obtenerCursos();
 
-  // Actualizar los datos cada 30 segundos
-  setInterval(obtenerCursos, 5000);
+// Actualizar los datos cada 30 segundos
+setInterval(obtenerCursos, 5000);  
